6b9d51ce0a228dc70f94e672b39e59236911cc80
Lein-Haz <M!yuki385>
2017-10-02 03:55:04 -0500

    Generic styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public testSize: number = 5;
+  public anArray = [];
   constructor() { }
 
   ngOnInit() {
+    for(let i = 0; i < this.testSize; i++){
+      this.anArray.push(2*i);
+    }
+    console.log(this.anArray);
+  }
+
+  more(){
+    let addCount = (this.anArray.length < 2)? 2 : this.anArray.length / 2;
+    this.delta(this.anArray.length + addCount);
+  }
+
+  less(){
+    if(this.anArray.length > 0){
+
+      let removeCount = (this.anArray.length < 3) ? this.anArray.length : 3;
+      console.log("Removing this many "+  removeCount);
+      this.delta(this.anArray.length - removeCount);
+    }
+
+  }
+
+  delta(newSize: number){
+    let removeFlag = (newSize < this.anArray.length);
+    let changeAmount = (newSize < this.anArray.length) ? this.anArray.length - newSize : newSize - this.anArray.length;
+    console.log(changeAmount);
+
+    for (let i = 0; i < changeAmount; i++){
+      if(removeFlag){
+        this.anArray.pop();
+
+      }else{
+        this.anArray.push((this.anArray.length - 1) * 2);
+
+      }
+    }
   }
 
 }
