77c8b4723a77512d06bdbe5a7fa2c18266437295
Lein-Haz <M!yuki385>
2017-10-03 15:51:40 -0500

    Shade styles!

--- src/core/services/constant.service.ts
+++ src/core/services/constant.service.ts
@@ -40,7 +40,9 @@ export class ConstantService {
 
   public static PICTURE_URLS = [
     'https://images.unsplash.com/photo-1428094479093-8973a318bd76?dpr=1&auto=compress,format&fm=jpg',
+    'https://images.unsplash.com/photo-1444882158336-bcfbd9298309?dpr=1&auto=compress,format&fm=jpg',
     'https://images.unsplash.com/photo-1435783099294-283725c37230?dpr=1&auto=compress,format&fm=jpg',
+    'https://images.unsplash.com/photo-1459664018906-085c36f472af?dpr=1&auto=compress,format&fm=jpg',
     'https://images.unsplash.com/photo-1429794890858-d3016a2bb73c?dpr=1&auto=compress,format&fm=jpg',
     'https://images.unsplash.com/photo-1490598000245-075175152d25?dpr=1&auto=compress,format&fm=jpg'
   ];
