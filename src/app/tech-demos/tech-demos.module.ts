ac3fac60ea811ace90dce1fd78af1abd3bc62273
Lein-Haz <M!yuki385>
2017-10-04 03:26:08 -0500

    Tile demo setup on tech page

--- src/app/tech-demos/tech-demos.module.ts
+++ src/app/tech-demos/tech-demos.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { TechDemosRoutingModule } from './tech-demos-routing.module';
 import { TechDemosComponent } from './tech-demos.component';
+import {AppMaterialModule} from "../shared/app-material/app-material.module";
+import {ScreenTileModule} from "../shared/screen-tile/screen-tile.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   imports: [
     CommonModule,
-    TechDemosRoutingModule
+    TechDemosRoutingModule,
+    AppMaterialModule,
+    ScreenTileModule,
+    FlexLayoutModule
   ],
   declarations: [TechDemosComponent]
 })
