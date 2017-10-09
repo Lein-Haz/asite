f021fd94aa8a0ba1211614ca48f5287fde3a36f6
Lein-Haz <M!yuki385>
2017-10-09 03:14:37 -0500

    Removed flexmodule again, text updates

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -5,15 +5,13 @@ import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import {AppMaterialModule} from "../shared/app-material/app-material.module";
 import {GoogleMapModule} from "../shared/google-map/google-map.module";
-import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   imports: [
     CommonModule,
     AboutRoutingModule,
     AppMaterialModule,
-    GoogleMapModule,
-    FlexLayoutModule
+    GoogleMapModule
   ],
   declarations: [AboutComponent]
 })
