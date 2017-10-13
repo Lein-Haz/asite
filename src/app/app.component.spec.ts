52699031b3b4eed0212118998b58f9c65eaf720e
Lein-Haz <M!yuki385>
2017-10-13 01:47:56 -0500

    Some app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {By} from "@angular/platform-browser";
+import {RouterOutlet} from "@angular/router";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -17,16 +23,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'asite'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('asite');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should have a router-outlet component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    let theRouter = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(theRouter).not.toBe(null);
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit{
 
   }
 
-  title = 'app';
+  title = 'asite';
 
   constructor(){
 
